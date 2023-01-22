0e5d160c6326af88cefc863ab3e66eb448805b49
jordenrabasco <jrabasc@ncsu.edu>
2023-01-22 12:11:06 -0500

    lint update: setup.py 1
    -lint update

--- setup.py
+++ setup.py
@@ -24,8 +24,7 @@ setup(
     },
     package_data={
         'q2_quality_control': ['citations.bib', 'assets/*'],
-        'q2_quality_control.tests': ['data/*',
-                           'data/expected/*'],
+        'q2_quality_control.tests': ['data/*', 'data/expected/*'],
         'q2_quality_control._threshold_graph': ['assets/index.html'],
     },
     zip_safe=False,

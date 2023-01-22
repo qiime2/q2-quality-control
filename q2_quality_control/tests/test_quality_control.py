dca6d3e0d929a8db310e62011b4eb18ead312450
jordenrabasco <jrabasc@ncsu.edu>
2023-01-22 11:44:27 -0500

    lint update: test_quality_control.py 4
    lint update

--- q2_quality_control/tests/test_quality_control.py
+++ q2_quality_control/tests/test_quality_control.py
@@ -976,24 +976,22 @@ class TestIdentify(TestPluginBase):
         temp_transposed_table = exp_table.transpose()
         temp_transposed_table = temp_transposed_table.dropna()
         exp_table = temp_transposed_table.transpose()
-        output_feature_table = decontam_identify(asv_or_otu_table=
-                                                 self.asv_table,
-                                                 meta_data=
-                                                 self.metadata_input,
-                                                 decon_method=
-                                                 'prevalence',
-                                                 prev_control_or_exp_sample_column
-                                                 ='Sample_or_ConTrol',
-                                                 prev_control_sample_indicator
-                                                 ='Control')
-        df_output_feature_table = transform(output_feature_table,
-                                            from_type=DecontamScoreFormat,
-                                            to_type=pd.DataFrame)
+        output_feature_table = decontam_identify(
+            asv_or_otu_table=self.asv_table,
+            meta_data=self.metadata_input,
+            decon_method='prevalence',
+            prev_control_or_exp_sample_column='Sample_or_ConTrol',
+            prev_control_sample_indicator='Control')
+        df_output_feature_table = transform(
+            output_feature_table,
+            from_type=DecontamScoreFormat,
+            to_type=pd.DataFrame)
         df_output_feature_table=df_output_feature_table.round(decimals=6)
         exp_table=exp_table.round(decimals=6)
         with tempfile.TemporaryDirectory() as temp_dir_name:
             test_biom_fp = os.path.join(temp_dir_name, 'test_output.tsv')
-            expected_biom_fp = os.path.join(temp_dir_name, 'expected_output.tsv')
+            expected_biom_fp = os.path.join(temp_dir_name,
+                                            'expected_output.tsv')
             df_output_feature_table.to_csv(test_biom_fp, sep="\t")
             exp_table.to_csv(expected_biom_fp, sep="\t")
             with open(test_biom_fp) as fh:
@@ -1010,19 +1008,21 @@ class TestIdentify(TestPluginBase):
         temp_transposed_table = exp_table.transpose()
         temp_transposed_table=temp_transposed_table.dropna()
         exp_table = temp_transposed_table.transpose()
-        output_feature_table = decontam_identify(asv_or_otu_table=self.asv_table,
-                                                 meta_data=self.metadata_input,
-                                                 decon_method='frequency',
-                                                 freq_concentration_column
-                                                 ='quant_reading')
-        df_output_feature_table = transform(output_feature_table,
-                                            from_type=DecontamScoreFormat,
-                                            to_type=pd.DataFrame)
+        output_feature_table = decontam_identify(
+            asv_or_otu_table=self.asv_table,
+            meta_data=self.metadata_input,
+            decon_method='frequency',
+            freq_concentration_column='quant_reading')
+        df_output_feature_table = transform(
+            output_feature_table,
+            from_type=DecontamScoreFormat,
+            to_type=pd.DataFrame)
         df_output_feature_table=df_output_feature_table.round(decimals=6)
         exp_table=exp_table.round(decimals=6)
         with tempfile.TemporaryDirectory() as temp_dir_name:
             test_biom_fp = os.path.join(temp_dir_name, 'test_output.tsv')
-            expected_biom_fp = os.path.join(temp_dir_name, 'expected_output.tsv')
+            expected_biom_fp = os.path.join(temp_dir_name,
+                                            'expected_output.tsv')
             df_output_feature_table.to_csv(test_biom_fp, sep="\t")
             exp_table.to_csv(expected_biom_fp, sep="\t")
             with open(test_biom_fp) as fh:
@@ -1038,23 +1038,23 @@ class TestIdentify(TestPluginBase):
         temp_transposed_table = exp_table.transpose()
         temp_transposed_table=temp_transposed_table.dropna()
         exp_table = temp_transposed_table.transpose()
-        output_feature_table = decontam_identify(asv_or_otu_table=self.asv_table,
-                                                 meta_data=self.metadata_input,
-                                                 decon_method='combined',
-                                                 prev_control_or_exp_sample_column
-                                                 ='Sample_or_ConTrol',
-                                                 prev_control_sample_indicator
-                                                 ='Control',
-                                                 freq_concentration_column
-                                                 ='quant_reading')
-        df_output_feature_table = transform(output_feature_table,
-                                            from_type=DecontamScoreFormat,
-                                            to_type=pd.DataFrame)
+        output_feature_table = decontam_identify(
+            asv_or_otu_table=self.asv_table,
+            meta_data=self.metadata_input,
+            decon_method='combined',
+            prev_control_or_exp_sample_column='Sample_or_ConTrol',
+            prev_control_sample_indicator='Control',
+            freq_concentration_column='quant_reading')
+        df_output_feature_table = transform(
+            output_feature_table,
+            from_type=DecontamScoreFormat,
+            to_type=pd.DataFrame)
         df_output_feature_table=df_output_feature_table.round(decimals=6)
         exp_table=exp_table.round(decimals=6)
         with tempfile.TemporaryDirectory() as temp_dir_name:
             test_biom_fp = os.path.join(temp_dir_name, 'test_output.tsv')
-            expected_biom_fp = os.path.join(temp_dir_name, 'expected_output.tsv')
+            expected_biom_fp = os.path.join(temp_dir_name,
+                                            'expected_output.tsv')
             df_output_feature_table.to_csv(test_biom_fp, sep="\t")
             exp_table.to_csv(expected_biom_fp, sep="\t")
             with open(test_biom_fp) as fh:
@@ -1079,13 +1079,15 @@ class TestRemove(TestPluginBase):
         exp_table = pd.read_csv(
             self.get_data_path('expected/no-contaminant-asv-table.tsv'),
             sep='\t', index_col=0)
-        output_asv_table = decontam_remove(asv_or_otu_table=self.asv_table,
-                                           decon_identify_table=self.identify_table,
-                                      threshold=0.1)
+        output_asv_table = decontam_remove(
+            asv_or_otu_table=self.asv_table,
+            decon_identify_table=self.identify_table,
+            threshold=0.1)
         temp_table = output_asv_table.to_dataframe()
         with tempfile.TemporaryDirectory() as temp_dir_name:
             test_biom_fp = os.path.join(temp_dir_name, 'test_output.tsv')
-            expected_biom_fp = os.path.join(temp_dir_name, 'expected_output.tsv')
+            expected_biom_fp = os.path.join(temp_dir_name,
+                                            'expected_output.tsv')
             temp_table.to_csv(test_biom_fp, sep="\t")
             exp_table.to_csv(expected_biom_fp, sep="\t")
             with open(test_biom_fp) as fh:
@@ -1097,4 +1099,3 @@ class TestRemove(TestPluginBase):
 
 if __name__ == '__main__':
     unittest.main()
-    

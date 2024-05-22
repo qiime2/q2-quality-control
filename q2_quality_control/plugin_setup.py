bf1a7d9ea03f1a4508fe45a50a6e45954c96a511
Greg Caporaso <192372+gregcaporaso@users.noreply.github.com>
2024-05-21 20:23:40 -0700

    adds filtering of contaminant sequences to decontam remove (#85)

--- q2_quality_control/plugin_setup.py
+++ q2_quality_control/plugin_setup.py
@@ -316,7 +316,7 @@ plugin.methods.register_function(
                 'prev_control_indicator': Str},
     outputs=[('decontam_scores', FeatureData[DecontamScore])],
     input_descriptions={
-        'table': ('ASV or OTU table which contaminate sequences '
+        'table': ('Feature table which contaminate sequences '
                   'will be identified from')
     },
     parameter_descriptions={
@@ -346,7 +346,7 @@ plugin.methods.register_function(
     output_descriptions={
         'decontam_scores': ('The resulting table of scores '
                             'from the decontam algorithm '
-                            'which scores each ASV or OTU on '
+                            'which scores each feature on '
                             'how likely they are to be a '
                             'contaminant sequence')
 
@@ -359,25 +359,29 @@ plugin.methods.register_function(
 plugin.methods.register_function(
     function=decontam_remove,
     inputs={'decontam_scores': FeatureData[DecontamScore],
-            'table': FeatureTable[Frequency]},
+            'table': FeatureTable[Frequency],
+            'rep_seqs': FeatureData[Sequence]},
     parameters={'threshold': Float},
-    outputs=[('filtered_table', FeatureTable[Frequency])],
+    outputs=[('filtered_table', FeatureTable[Frequency]),
+             ('filtered_rep_seqs', FeatureData[Sequence])],
     input_descriptions={
-        'decontam_scores': ('Output table from decontam identify'),
-        'table': ('ASV or OTU table which contaminate sequences '
-                  'will be identified from')
+        'decontam_scores': ('Pre-feature decontam scores.'),
+        'table': ('Feature table from which contaminants will be removed.'),
+        'rep_seqs': ('Feature representative sequences from which '
+                     'contaminants will be removed.')
     },
     parameter_descriptions={
-        'threshold': ('Select threshold cutoff for decontam algorithm scores')
+        'threshold': ('Decontam score threshold. Features with a score less '
+                      'than or equal to this threshold will be removed.')
     },
     output_descriptions={
-        'filtered_table': ('The resulting feature table of scores '
-                           'once contaminants are removed')
-
+        'filtered_table': ('Feature table with contaminants removed.'),
+        'filtered_rep_seqs': ('Feature representative sequences with '
+                              'contaminants removed.')
     },
-    name='Removes contaminant',
-    description=('This method removes contaminant sequences from an '
-                 'OTU or ASV table and returns the amended table to the user')
+    name='Remove contaminants',
+    description=('Remove contaminant sequences from a feature table and '
+                 'the associated representative sequences.')
 )
 
 
@@ -428,7 +432,7 @@ plugin.pipelines.register_function(
              ('score_histograms', Visualization)
              ],
     input_descriptions={
-        'table': ('ASV or OTU table which contaminate sequences '
+        'table': ('Feature table which contaminate sequences '
                   'will be identified from')
     },
     parameter_descriptions={
@@ -470,7 +474,7 @@ plugin.pipelines.register_function(
             'and parameter split_column values should be written.'),
         'decontam_scores': ('The resulting table of scores '
                             'from the decontam algorithm '
-                            'which scores each ASV or OTU on '
+                            'which scores each feature on '
                             'how likely they are to be a '
                             'contaminant sequence'),
         'score_histograms': (
@@ -484,7 +488,6 @@ plugin.pipelines.register_function(
         'identifies contaminant sequences from an '
         'OTU or ASV table and reports them to the user')
 )
-
 plugin.register_formats(DecontamScoreFormat, DecontamScoreDirFmt)
 plugin.register_semantic_types(DecontamScore)
 plugin.register_semantic_type_to_format(
--- q2_quality_control/tests/test_decontam.py
+++ q2_quality_control/tests/test_decontam.py
@@ -7,7 +7,9 @@
 # ----------------------------------------------------------------------------
 import unittest
 import pandas as pd
+import pandas.testing as pdt
 import qiime2
+import qiime2.plugin.util
 import biom
 from qiime2.plugin.testing import TestPluginBase
 from q2_quality_control.decontam import (decontam_identify,
@@ -109,38 +111,95 @@ class TestIdentify(TestPluginBase):
             self.assertEqual(test_table, expecter_table)
 
 
+def _sort_seqs(seqs):
+    return sorted(list(seqs), key=lambda x: x.metadata['id'])
+
+
 class TestRemove(TestPluginBase):
     package = 'q2_quality_control.tests'
 
     def setUp(self):
         super().setUp()
-        table = qiime2.Artifact.load(
-            self.get_data_path('expected/decon_default_ASV_table.qza'))
-        self.asv_table = table.view(qiime2.Metadata).to_dataframe()
-        id_table = qiime2.Artifact.load(
-            self.get_data_path('expected/decon_default_score_table.qza'))
-        self.identify_table = id_table.view(qiime2.Metadata)
+        self.input_table = pd.DataFrame(
+            [[1, 2, 3, 4, 5], [9, 10, 11, 12, 13]],
+            columns=['abc', 'def', 'jkl', 'mno', 'pqr'],
+            index=['sample-1', 'sample-2'])
+        self.input_seqs = pd.Series(
+            ['ACGT', 'TTTT', 'AAAA', 'CCCC', 'GGG'],
+            index=['abc', 'def', 'jkl', 'mno', 'pqr'])
+        self.input_scores = pd.DataFrame(
+            [[13.0, 0.969179],
+             [16.0, 0.566067],
+             [25.0, 0.019475],
+             [10.0, 0.383949],
+             [13.0, 0.969179]],
+            index=['abc', 'def', 'jkl', 'mno', 'pqr'],
+            columns=['prev', 'p'])
 
     def test_remove(self):
-        exp_table = pd.read_csv(
-            self.get_data_path('expected/no-contaminant-asv-table.tsv'),
-            sep='\t', index_col=0)
-        output_asv_table = decontam_remove(
-            table=self.asv_table,
-            decontam_scores=self.identify_table,
-            threshold=0.1)
-        temp_table = output_asv_table.to_dataframe()
-        with tempfile.TemporaryDirectory() as temp_dir_name:
-            test_biom_fp = os.path.join(temp_dir_name, 'test_output.tsv')
-            expected_biom_fp = os.path.join(temp_dir_name,
-                                            'expected_output.tsv')
-            temp_table.to_csv(test_biom_fp, sep="\t")
-            exp_table.to_csv(expected_biom_fp, sep="\t")
-            with open(test_biom_fp) as fh:
-                test_table = biom.Table.from_tsv(fh, None, None, None)
-            with open(expected_biom_fp) as th:
-                expecter_table = biom.Table.from_tsv(th, None, None, None)
-            self.assertEqual(test_table, expecter_table)
+        exp_table = pd.DataFrame(
+            [[1, 2, 4, 5], [9, 10, 12, 13]],
+            columns=['abc', 'def', 'mno', 'pqr'],
+            index=['sample-1', 'sample-2'])
+        exp_seqs = pd.Series(['ACGT', 'TTTT', 'CCCC', 'GGG'],
+                             index=['abc', 'def', 'mno', 'pqr'])
+
+        obs_table, obs_seqs = decontam_remove(
+            table=self.input_table,
+            decontam_scores=self.input_scores,
+            threshold=0.1,
+            rep_seqs=self.input_seqs)
+
+        pdt.assert_series_equal(obs_seqs, exp_seqs)
+        pdt.assert_frame_equal(obs_table, exp_table)
+
+    def test_remove_alt_threshold(self):
+        exp_table = pd.DataFrame(
+            [[1, 2, 5], [9, 10, 13]],
+            columns=['abc', 'def', 'pqr'],
+            index=['sample-1', 'sample-2'])
+        exp_seqs = pd.Series(['ACGT', 'TTTT', 'GGG'],
+                             index=['abc', 'def', 'pqr'])
+
+        obs_table, obs_seqs = decontam_remove(
+            table=self.input_table,
+            decontam_scores=self.input_scores,
+            threshold=0.383949,
+            rep_seqs=self.input_seqs)
+
+        pdt.assert_series_equal(obs_seqs, exp_seqs)
+        pdt.assert_frame_equal(obs_table, exp_table)
+
+    def test_remove_all(self):
+        exp_table = pd.DataFrame(
+            [], columns=[], index=['sample-1', 'sample-2'])
+        exp_seqs = pd.Series([], index=[])
+
+        obs_table, obs_seqs = decontam_remove(
+            table=self.input_table,
+            decontam_scores=self.input_scores,
+            threshold=1.0,
+            rep_seqs=self.input_seqs)
+
+        pdt.assert_series_equal(obs_seqs, exp_seqs, check_dtype=False)
+        pdt.assert_frame_equal(obs_table, exp_table, check_dtype=False)
+
+    def test_remove_none(self):
+        exp_table = pd.DataFrame(
+            [[1, 2, 3, 4, 5], [9, 10, 11, 12, 13]],
+            columns=['abc', 'def', 'jkl', 'mno', 'pqr'],
+            index=['sample-1', 'sample-2'])
+        exp_seqs = pd.Series(['ACGT', 'TTTT', 'AAAA', 'CCCC', 'GGG'],
+                             index=['abc', 'def', 'jkl', 'mno', 'pqr'])
+
+        obs_table, obs_seqs = decontam_remove(
+            table=self.input_table,
+            decontam_scores=self.input_scores,
+            threshold=0.0,
+            rep_seqs=self.input_seqs)
+
+        pdt.assert_series_equal(obs_seqs, exp_seqs)
+        pdt.assert_frame_equal(obs_table, exp_table)
 
 
 class TestIdentify_mixed_names(TestPluginBase):
